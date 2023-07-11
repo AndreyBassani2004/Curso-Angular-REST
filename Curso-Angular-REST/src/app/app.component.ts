92b83fdaa2dfb8a1fc07afd33ae07c03cb82e18d
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-07-11 09:48:41 -0300

    Criando Routers [Rotas] para a nossa Home.

--- Curso-Angular-REST/src/app/app.component.ts
+++ Curso-Angular-REST/src/app/app.component.ts
@@ -11,12 +11,5 @@ import { LoginServiceService } from './service/login-service.service';
 export class AppComponent {
   title = 'Curso-Angular-REST';
 
-  usuario = {login: '', senha: ''};
-
-  constructor(private loginService: LoginServiceService){}
-
-  public login(){
-    this.loginService.login(this.usuario);
-  }
 
 }
--- Curso-Angular-REST/src/app/app.module.ts
+++ Curso-Angular-REST/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { LoginServiceService } from './service/login-service.service';
+import { LoginComponent } from './login/login.component';
 
+export const appRouters: Routes = [
+  {path : 'home', component : HomeComponent},
+  {path : 'login', component : LoginComponent},
+  {path : '', component : LoginComponent}
+];
+
+export const routes : ModuleWithProviders = RouterModule.forRoot(appRouters);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
